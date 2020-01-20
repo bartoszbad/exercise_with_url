cb584b0e5654c41ae2fdff55ff0588df4d19aef5
Bartosz Badowski <bartoszbad@gmail.com>
2020-01-20 22:39:24 +0100

    rename2

--- URL/settings.py
+++ URL/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shortening_urls.apps.RecruitmentTaskConfig',
+    'shortening_urls.apps.ShorteningUrlsConfig',
 ]
 
 MIDDLEWARE = [
--- shortening_urls/apps.py
+++ shortening_urls/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class RecruitmentTaskConfig(AppConfig):
+class ShorteningUrlsConfig(AppConfig):
     name = 'shortening_urls'
